d61b34ba7d022741bb42ab67589f37d0a9fd3462
Ulug`bek <avazov0989@gmail.com>
2020-09-14 00:20:19 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,62 +1,92 @@
 AOS.init({
   offset: 100,
   duration: 800,
-  easing: 'ease-in-out',
-  once : true
+  easing: "ease-in-out",
+  once: true,
 });
 
-$(function() {
+$(function () {
+  $(".spinner").fadeOut("slow");
 
-  $('.spinner').fadeOut('slow');
-
-  let navbar = document.getElementById('navbar');
-  window.addEventListener('scroll', () => {
-    if(window.pageYOffset > 50) {
-      navbar.classList.remove('dsabl');
-      navbar.classList.add('actv');
-      $('.goTop').fadeIn();
+  let navbar = document.getElementById("navbar");
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 50) {
+      navbar.classList.remove("dsabl");
+      navbar.classList.add("actv");
+      $(".goTop").fadeIn();
     } else {
-      navbar.classList.remove('actv');
-      navbar.classList.add('dsabl');
-      $('.goTop').fadeOut();
+      navbar.classList.remove("actv");
+      navbar.classList.add("dsabl");
+      $(".goTop").fadeOut();
     }
   });
 
-  $('.goTop').click(function() {
-    scroll(0, 0)
+  $(".goTop").click(function () {
+    scroll(0, 0);
   });
 
-  $('.navbar-toggler').click(()=>{
-    if(window.pageYOffset < 50) {
-      navbar.classList.toggle('actv');
-      navbar.classList.toggle('dsabl');
+  $(".navbar-toggler").click(() => {
+    if (window.pageYOffset < 50) {
+      navbar.classList.toggle("actv");
+      navbar.classList.toggle("dsabl");
     }
   });
 
-  let navLink = $('.nav-link');
-  window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 0 && window.pageYOffset <= 580) {
-      clearLink();
-      navLink[0].classList.add('actives');
+  let navLink = $(".nav-link");
+
+  window.addEventListener("scroll", () => {
+    if (window.innerWidth < 780) {
+      if (window.pageYOffset > 0 && window.pageYOffset <= 380) {
+        clearLink();
+        navLink[0].classList.add("actives");
+      }
+    } else {
+      if (window.pageYOffset > 0 && window.pageYOffset <= 580) {
+        clearLink();
+        navLink[0].classList.add("actives");
+      }
     }
-    if(window.pageYOffset > 580 && window.pageYOffset <= 1750) {
-      clearLink();
-      navLink[1].classList.add('actives');
+
+    if (window.innerWidth < 780) {
+      if (window.pageYOffset > 380 && window.pageYOffset <= 4700) {
+        clearLink();
+        navLink[1].classList.add("actives");
+      }
+    } else {
+      if (window.pageYOffset > 580 && window.pageYOffset <= 1750) {
+        clearLink();
+        navLink[1].classList.add("actives");
+      }
     }
-    if(window.pageYOffset > 2200 && window.pageYOffset <= 2500) {
-      clearLink();
-      navLink[2].classList.add('actives');
+
+    if (window.innerWidth < 780) {
+      if (window.pageYOffset > 4700 && window.pageYOffset <= 7200) {
+        clearLink();
+        navLink[2].classList.add("actives");
+      }
+    } else {
+      if (window.pageYOffset > 2200 && window.pageYOffset <= 2500) {
+        clearLink();
+        navLink[2].classList.add("actives");
+      }
     }
-    if(window.pageYOffset > 3100) {
-      clearLink();
-      navLink[3].classList.add('actives');
+
+    if (window.innerWidth < 780) {
+      if (window.pageYOffset > 7200) {
+        clearLink();
+        navLink[3].classList.add("actives");
+      }
+    } else {
+      if (window.pageYOffset > 3100) {
+        clearLink();
+        navLink[3].classList.add("actives");
+      }
     }
-  })
+  });
 
   function clearLink() {
-    for(let i = 0; i < navLink.length; i++) {
-      navLink[i].classList.remove('actives');
+    for (let i = 0; i < navLink.length; i++) {
+      navLink[i].classList.remove("actives");
     }
   }
-
 });
