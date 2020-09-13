d5e27095bd3a5e75b961cb7821d5c515ae2e6a09
Ulug`bek <avazov0989@gmail.com>
2020-09-13 23:32:54 +0500

    update

--- js/main.js
+++ js/main.js
@@ -20,16 +20,43 @@ $(function() {
       navbar.classList.add('dsabl');
       $('.goTop').fadeOut();
     }
-  })
+  });
 
   $('.goTop').click(function() {
     scroll(0, 0)
-  })
+  });
 
   $('.navbar-toggler').click(()=>{
     if(window.pageYOffset < 50) {
       navbar.classList.toggle('actv');
       navbar.classList.toggle('dsabl');
     }
+  });
+
+  let navLink = $('.nav-link');
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 0 && window.pageYOffset <= 580) {
+      clearLink();
+      navLink[0].classList.add('actives');
+    }
+    if(window.pageYOffset > 580 && window.pageYOffset <= 1750) {
+      clearLink();
+      navLink[1].classList.add('actives');
+    }
+    if(window.pageYOffset > 2200 && window.pageYOffset <= 2500) {
+      clearLink();
+      navLink[2].classList.add('actives');
+    }
+    if(window.pageYOffset > 3100) {
+      clearLink();
+      navLink[3].classList.add('actives');
+    }
   })
+
+  function clearLink() {
+    for(let i = 0; i < navLink.length; i++) {
+      navLink[i].classList.remove('actives');
+    }
+  }
+
 });
